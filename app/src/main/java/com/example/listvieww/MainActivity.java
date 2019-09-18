203c4a171e5e8bdd04d9c62ddc94b8064f527df0
liemb <lb6031@bs.amalnet.k12.il>
2019-09-18 11:13:56 +0300

    design

--- app/src/main/java/com/example/listvieww/MainActivity.java
+++ app/src/main/java/com/example/listvieww/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.listvieww;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,9 +8,10 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    String [] lands= {"Asia","Europe","Africa","Antartica","Australia","South America","North America"};
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    String[] lands = {"Asia", "Europe", "Africa", "Antartica", "Australia", "South America", "North America"};
     ListView lv;
     String st;
 
@@ -17,18 +19,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lv=(ListView) findViewById(R.id.lv);
+        lv = (ListView) findViewById(R.id.lv);
+
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(this.layout.support_simple_spinner_dropdown_item, lands);
+        lv.setAdapter(adp);
 
         lv.setOnItemClickListener(this);
         lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
-        ArrayAdapter<String> adp = new ArrayAdapter<String> (this.layout.support_simple_spinner_dropdown_item, lands);
-        lv.setAdapter(adp);
-
     }
 
     @Override
-    public void onItemClick (AdapterView<?> parent), View view, int position, long id){
-        st=lands[position];
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(this, "" +lands[position],Toast.LENGTH_SHORT).show();
+        st = lands[position];
     }
 
+    public void click(View view) {
+        if (st.length()!=0){
+            Intent si = new Intent (this,Main2Activity.class);
+            si.putExtra("st",st);
+            startActivity(si);
+        }
+    }
+}
