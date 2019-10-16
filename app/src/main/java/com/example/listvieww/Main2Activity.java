a95ca9ddfcb595159e1b22b267a87f3d16394f1f
liem <lb6031@bs.amalnet.k12.il>
2019-10-16 19:40:50 +0300

    done, fixed with javadocs

--- app/src/main/java/com/example/listvieww/Main2Activity.java
+++ app/src/main/java/com/example/listvieww/Main2Activity.java
@@ -9,10 +9,21 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+/**
+ * On this Activity the user needs to choose one country (from his chosen continent) out of 7.
+ * After choosing a country the user will see its Capital city and the population of this country.
+ */
+
 public class Main2Activity extends AppCompatActivity implements AdapterView.OnItemClickListener  {
 
     TextView tv1,tv2;
     ListView lv;
+
+    /**
+     * creating two two-dimensional arrays.
+     * the first one contains the contries in each continent, each sell is a different continent.
+     * The first cell of the second array contains the Capital cities and the second contains the populations, in the same order of the countries.
+     */
     String[][]st1={{"Israel","India","China","Yemen","Jordan","Kuwait","Thailand"},{"Italy","France","Spain","United Kingdom","Germany","Poland","Belgium"},{"Morocco","Algeria","Libya","Chad","Egypt","Kenia","Sudan"},{"Brazil","Bolivia","Paraguay","Argentina","Colombia","Ecuador","Peru"},{"Canada","Mexico","Barbados","Costa Rica","Bahamas","Jamaica","California"}};
     String[][]st2={{"Jerusalem","New Deli","Beijing","Sana'a","Amman","Kuwait City","Bangkok","Rome","Paris","Madrid","London","Berlin","WarSaw","Brussels","Rabat","Algiers","Tripoli","N'Djamena","Cairo","Nairobi","Khartoum","Brasilia","La Paz","Asuncion","Buenos Aires","Bogota","Quito","Lima","Ottawa","Mexico City","Bridgetown","San Jose","Nassau","Kingston","Sacramento"},
             {"8,891,800","1,349,217,956","1,409,517,397","28,250,420","10,428,241","4,621,638","66,188,503","60,359,546","67,022,000","46,733,038","67,545,757","82,979,100","38,433,600","11,420,163","36,029,138","42,200,000","7,200,000","15,228,584","94,798,827","49,364,325","41,592,539","210,147,125","11,428,245","7,152,703","44,938,712","48,258,494","16,624,858","33,105,273","36,708,083","126,577,691","285,719","4,905,769","395,361","2,890,299","39,557,045"}};
@@ -25,18 +36,28 @@ public class Main2Activity extends AppCompatActivity implements AdapterView.OnIt
         tv1=(TextView)findViewById(R.id.capi);
         tv2=(TextView)findViewById(R.id.popu);
         lv=(ListView)findViewById(R.id.lv);
+
+        /**
+         * getting the user's continent choice from MainActivity.
+         */
         Intent si=getIntent();
         x=si.getIntExtra("position",2);
 
         ArrayAdapter<String> adp=new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,st1[x]);
-
+        /**
+         * connecting the ListView to the String array that the user picked.
+         */
         lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         lv.setOnItemClickListener(this);
         lv.setAdapter(adp);
     }
 
     @Override
+
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        /**
+         * showing as TextView the capital city and the population of the chosen country.
+         */
         tv1.setText("Capital City: " + st2[0][x * 7 + position]);
         tv2.setText("Population: " + st2[1][x * 7 + position]);
     }
--- app/src/main/java/com/example/listvieww/MainActivity.java
+++ app/src/main/java/com/example/listvieww/MainActivity.java
@@ -10,20 +10,30 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+/**
+ * @author Liem Buchuk
+ * @since 15.10.19
+ * On this activity the user needs to chose one continent out of 5, with the ability to change his choice.
+ * The chosen one will pop as a Toast.
+ */
+
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
     String[] lands = {"Asia", "Europe", "Africa", "South America", "North America"};
     ListView lv;
-    int y=60;
+    int y=-1;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lv = (ListView) findViewById(R.id.lv);
-
         lv.setOnItemClickListener(this);
         lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-
+        /**
+         * connenting an adaptar to the string array
+         */
         ArrayAdapter<String> adp = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item, lands);
         lv.setAdapter(adp);
 
@@ -32,13 +42,26 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
     @Override
+    /**
+     * Showing a Toast with the chosen continent and saving the choice number in 'y'.
+     */
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Toast.makeText(this, "" +lands[position],Toast.LENGTH_SHORT).show();
         y=position;
     }
 
     public void click(View view) {
-        if (y!=60){
+        /**
+         * when the Button is clicked,
+         * if the user chose a continent this sends him to Main2Activity with the continent that he chose.
+         * if the user did not choose any continent a Toast will be shown telling him to chose one.
+         */
+        if (y==-1){
+            Toast toast2;
+            toast2 =Toast.makeText(this, "please choose",Toast.LENGTH_SHORT);
+            toast2.show();
+        }
+        else{
             Intent si = new Intent (this,Main2Activity.class);
             si.putExtra("position",y);
             startActivity(si);
